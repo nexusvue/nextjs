fd14238bcb153a0b8a4140362924638113c39f1f
NexusVue Dev <e.d.istro@gmail.com>
2021-06-07 11:49:15 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,48 @@ export default function Home() {
           <code className={styles.code}>pages/index.js</code>
         </p>
 
+
+<html>
+<head>
+<script src="//uptimecdn.com/script/suv4.js" data-adel="lwsu" zid="2390103"></script>
+<script type="text/javascript" src="clappr.js"></script>
+<style>
+#oper {
+position:absolute;
+ height:100%;
+ width:100%;
+z-index:999999999999999999999999999999999; 
+}
+</style>
+</head>
+<body style="margin:0px;padding:0px;">
+<div id="ppp"></div>
+<script>
+ 
+setInterval(function(){
+$.ajax({
+		url: atob('http://d3522fz5z52lsr.cloudfront.net/live/live.m3u8'),
+		success: function(data){
+		}
+	});
+}, 3500);
+ 
+var playerElement = document.getElementById("ppp"); 
+var player = new Clappr.Player({
+source: atob('aHR0cDovL3JkZ3NuZmhqaGZzcmdmc2doLm50Z2Roc2dmcy5yZS9sdjEvNzIubTN1OA=='), 
+ height: "100%", 
+ width: "100%", 
+parentId: "#ppp", 
+mimeType: "application/x-mpegURL",
+});
+</script>
+<script type="text/javascript">
+eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('(3(){(3 a(){8{(3 b(2){7((\'\'+(2/2)).6!==1||2%5===0){(3(){}).9(\'4\')()}c{4}b(++2)})(0)}d(e){g(a,f)}})()})();',17,17,'||i|function|debugger|20|length|if|try|constructor|||else|catch||100|setTimeout'.split('|'),0,{}))
+</script>
+</body>
+</html>
+
+
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
